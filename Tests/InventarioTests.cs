e5b3b8d9600644aed81c19651be1e7a34a30b5ee
nargotik <a17442@alunos.ipca.pt>
2019-05-15 10:49:21 +0100

    Adicionado testing

---
+++ Tests/InventarioTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ITgestao;
+
+namespace Tests
+{
+    [TestClass]
+    public class InventarioTests
+    {
+        [TestMethod]
+        public void Create_Inventario()
+        {
+            int id = 20;
+            int expected = 20;
+            var _inv = new Inventario(id);
+            Assert.AreEqual(_inv.Empresa, expected);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(IdBadException))]
+        public void Create_Inventario_IdBad_Trows()
+        {
+            int id = -20;
+            var _inv = new Inventario(id);
+        }
+    }
+}
---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79bba03c-110a-4405-94cc-8dab5e4f1073")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
