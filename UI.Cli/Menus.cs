60c391c743c76a815cea7e47cafce5a06237bcfb
Óscar Silva <a14383@alunos.ipca.pt>
2019-06-03 15:44:30 +0100

    Add menu class

---
+++ UI.Cli/Menus.cs
@@ -0,0 +1,55 @@
+﻿// -------------------------------------------------
+// <copyright file="UI.Cli.cs" company="IPCA">
+// </copyright>
+// <summary>
+//	LP2 - 2018-2019
+// <desc></desc>
+// </summary>
+//-------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Cli
+{
+    class Menus
+    {
+
+        public static int RendeMenu(Dictionary<int, string> _menu)
+        {
+            MostraMenu(_menu);
+
+
+            return LeOpcaoMenu(_menu);
+        }
+
+        static void MostraMenu(Dictionary<int, string> _menu)
+        {
+            foreach (KeyValuePair<int, string> menuItem in _menu)
+            {
+                Console.WriteLine("{0} - {1}", menuItem.Key, menuItem.Value);
+            }
+
+            Console.ReadKey();
+        }
+
+        static int LeOpcaoMenu(Dictionary<int, string> _menu)
+        {
+            int opcao;
+            string value;
+
+            do
+            {
+                Console.Write("Opção:");
+                opcao = Console.Read();
+            }
+            while (!_menu.TryGetValue(opcao, out value) == true);
+
+            return opcao;
+        }
+        
+     }
+}
