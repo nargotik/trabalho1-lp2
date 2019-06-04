69d9460bf959ca9ce32d15d78cc082ed343c4d2c
Óscar Silva <a14383@alunos.ipca.pt>
2019-06-04 16:01:08 +0100

    Exception .cs Documentation

--- ITGestao/App/Exceptions.cs
+++ ITGestao/App/Exceptions.cs
@@ -3,7 +3,7 @@
 // </copyright>
 // <summary>
 // LP2 - 2018-2019
-// <desc>Classe abstrata que define e cria objetos do tipo Item. Armazena e trata objetos do tipo Item o mais genéricos possível</desc>
+// <desc>Classes que permitem lidar com erros que ocorrem durante a execução da aplicação</desc>
 // </summary>
 //-------------------------------------------------
 
@@ -15,6 +15,9 @@ using System.Threading.Tasks;
 
 namespace ITgestao
 {
+    /// <summary>
+    /// Impede a existência de ID duplicados
+    /// </summary>
     public class IdDuplicatedException : ApplicationException
     {
         public IdDuplicatedException(string _s = "Id Duplicado") : base(_s)
@@ -23,6 +26,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Evita a inserção de ID inválido
+    /// </summary>
     public class IdBadException : ApplicationException
     {
         public IdBadException(string _s = "Id não válido") : base(_s)
@@ -31,6 +37,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Impede a iniciação de objetos com argumentos em falta
+    /// </summary>
     public class InitBadException : ApplicationException
     {
         public InitBadException(string _s = "Inicialização do objecto com argumentos em falta") : base(_s)
@@ -39,6 +48,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Evita a inserção de Itens inválidos
+    /// </summary>
     public class ItemInvalido : ApplicationException
     {
         public ItemInvalido(string _s = "Item Inválido") : base(_s)
@@ -47,6 +59,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Indica que não foi possível efetuar o pedido solicitado
+    /// </summary>
     public class NotImplementedException : ApplicationException
     {
         public NotImplementedException(string _s = "Não implementado") : base(_s)
@@ -55,6 +70,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Impede a manipulação de um Equipamento inválido
+    /// </summary>
     public class InvalidEquipamentoException : ApplicationException
     {
         public InvalidEquipamentoException(string _s = "Não implementado") : base(_s)
@@ -63,6 +81,9 @@ namespace ITgestao
         }
     }
 
+    /// <summary>
+    /// Informa que o item não existe
+    /// </summary>
     public class NotExists : ApplicationException
     {
         public NotExists(string _s = "Item não existe") : base(_s)
