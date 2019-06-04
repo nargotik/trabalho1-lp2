104a47c2318b4412754a3fd862d37d965feec259
nargotik <a17442@alunos.ipca.pt>
2019-06-04 11:29:36 +0100

    changes

--- UI.Cli/Program.cs
+++ UI.Cli/Program.cs
@@ -23,74 +23,81 @@ namespace UI.Cli
             Input.LeString("Prima qualquer tecla para terminar.");
         }
 
+        /// <summary>
+        /// Constroi os menus e respectivos callbacks
+        /// </summary>
         static void ConstroiMenus()
         {
-            Menu principal  = Menu.getInstance(0);
-            Menu inventario0 = Menu.getInstance(1);
-            Menu localizacoes = Menu.getInstance(2);
-            Menu inventario1 = Menu.getInstance(3);
-            Menu pesquisa0 = Menu.getInstance(4);
+            Menu menuprincipal  = Menu.getInstance(0);
+            Menu menuinventario0 = Menu.getInstance(1);
+            Menu menulocalizacoes = Menu.getInstance(2);
+            Menu menuinventario1 = Menu.getInstance(3);
+            Menu menupesquisa0 = Menu.getInstance(4);
+            Menu menupesquisa1 = Menu.getInstance(5);
 
             #region MENU INICIAL
-            principal.Descricao = "======================= Menu Inicial =======================";
+            menuprincipal.Descricao = "======================= Menu Inicial =======================";
 
-            principal
-                .Add("- Inventário Armazem 0", () => inventario0.RendeMenu())
-                .Add("- Inventário Armazem 1", () => inventario1.RendeMenu())
-                .Add("- Localizacoes", () => localizacoes.RendeMenu())
+            menuprincipal
+                .Add("- Inventário Armazem 0", () => menuinventario0.RendeMenu())
+                .Add("- Inventário Armazem 1", () => menuinventario1.RendeMenu())
+                .Add("- Localizacoes", () => menulocalizacoes.RendeMenu())
                 .Add("+ Sair");
             #endregion
 
             #region MENU INVENTARIO
-            inventario1.Descricao = "======================= INVENTARIO: Armazem 1 =======================";
-            inventario1
-                .Add("Adicionar Item", () => AdicionaItem(1))
-                .Add("Editar Item", () => EditaItem(1))
-                .Add("Remover Item", () => RemoveItem(1))
-                .Add("Listar Items", () => ListaItems(1))
-                .Add("Pesquisa Items", () => ListaItems(1))
-                .Add("Mostra Total de Items", () => ContaItems(1))
-                .Add("Eliminar tudo", () => RemoveItems(1))
-                .Add("< Voltar", () => principal.RendeMenu());
+            menuinventario1.Descricao = "======================= INVENTARIO: Armazem 1 =======================";
+            menuinventario1
+                .Add("Adicionar Item", () => AdicionaItem(1,menuinventario1))
+                .Add("Editar Item", () => EditaItem(1, menuinventario1))
+                .Add("Remover Item", () => RemoveItem(1, menuinventario1))
+                .Add("Listar Items", () => ListaItems(1, menuinventario1))
+                .Add("Mostra Total de Items", () => ContaItems(1, menuinventario1))
+                .Add("Eliminar tudo", () => RemoveItems(1, menuinventario1))
+                .Add("+ Pesquisa Filtros", () => menupesquisa1.RendeMenu() )
+                .Add("< Voltar", () => menuprincipal.RendeMenu());
             #endregion
 
             #region MENU INVENTARIO Empresa 0
-            inventario0.Descricao = "======================= INVENTARIO: Armazem 0 =======================";
-            inventario0
-                .Add("Adicionar Item", () => AdicionaItem(0))
-                .Add("Editar Item", () => EditaItem(0))
-                .Add("Remover Item", () => RemoveItem(0))
-                .Add("Listar Items", () => ListaItems(0))
-                .Add("Mostra Total de Items", () => ContaItems(0))
-                .Add("Eliminar tudo" , () => RemoveItems(0) )
-                .Add("< Voltar", () => principal.RendeMenu());
+            menuinventario0.Descricao = "======================= INVENTARIO: Armazem 0 =======================";
+            menuinventario0
+                .Add("Adicionar Item", () => AdicionaItem(0, menuinventario0))
+                .Add("Editar Item", () => EditaItem(0, menuinventario0))
+                .Add("Remover Item", () => RemoveItem(0, menuinventario0))
+                .Add("Listar Items", () => ListaItems(0, menuinventario0))
+                .Add("Mostra Total de Items", () => ContaItems(0, menuinventario0))
+                .Add("Eliminar tudo" , () => RemoveItems(0, menuinventario0) )
+                .Add("+ Pesquisa Filtros", () => menupesquisa0.RendeMenu())
+                .Add("< Voltar", () => menuprincipal.RendeMenu());
             #endregion
 
             #region MENU LOCALIZACOES
-            localizacoes.Descricao = "======================= LOCALIZACOES =======================";
-            localizacoes
-                .Add("Adicionar Localizacao")
-                .Add("Remover Localizacao")
-                .Add("< Voltar", () => principal.RendeMenu());
+            menulocalizacoes.Descricao = "======================= LOCALIZACOES =======================";
+            menulocalizacoes
+                .Add("Adicionar Localizacao", () => AdicionaLocalizacao())
+                .Add("Remover Localizacao", () => RemoveLocalizacao())
+                .Add("Edita Localizacao", () => AlteraLocalizacao())
+                .Add("Lista Localizacoes", () => ListaLocalizacao())
+                .Add("< Voltar", () => menuprincipal.RendeMenu());
             #endregion
 
             #region MENU PESQUISA
-            pesquisa0.Descricao = "======================= PESQUISA: Armazem 0 =======================";
-            pesquisa0
-                .Add("Pesquisa por Nome")
-                .Add("Perquisar por Id")
-                .Add("Perquisar por Data de Fabrico")
-                .Add("Perquisar por Tipo")
-                .Add("< Voltar", () => principal.RendeMenu());
+            menupesquisa0.Descricao = "======================= PESQUISA: Armazem 0 =======================";
+            menupesquisa0
+                .Add("Pesquisa por Nome", () => PesquisaNome(0, menupesquisa0))
+                .Add("Perquisar por Id", () => PesquisaId(0, menupesquisa0))
+                .Add("Perquisar por Data de Fabrico", () => PesquisaFabrico(0, menupesquisa0))
+                .Add("Perquisar por Tipo", () => PesquisaTipo(0, menupesquisa0))
+                .Add("< Voltar", () => menuinventario0.RendeMenu());
             #endregion
             #region MENU PESQUISA
-            pesquisa0.Descricao = "======================= PESQUISA: Armazem 0 =======================";
-            pesquisa0
-                .Add("Pesquisa por Nome")
-                .Add("Perquisar por Id")
-                .Add("Perquisar por Data de Fabrico")
-                .Add("Perquisar por Tipo")
-                .Add("< Voltar", () => principal.RendeMenu());
+            menupesquisa1.Descricao = "======================= PESQUISA: Armazem 1 =======================";
+            menupesquisa1
+                .Add("Pesquisa por Nome", () => PesquisaNome(0, menupesquisa1))
+                .Add("Perquisar por Id", () => PesquisaId(0, menupesquisa1))
+                .Add("Perquisar por Data de Fabrico", () => PesquisaFabrico(0, menupesquisa1))
+                .Add("Perquisar por Tipo", () => PesquisaTipo(0, menupesquisa1))
+                .Add("< Voltar", () => menuinventario1.RendeMenu());
             #endregion
         }
 
@@ -103,49 +110,104 @@ namespace UI.Cli
             Menu.getInstance(_id).RendeMenu();
         }
 
-        static void AdicionaItem(int _entidade)
+        #region ===============================================INVENTARIO
+        static void AdicionaItem(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Adiciona Item =======================");
-            Menu.getInstance(1).RendeMenu();
+            GoBack(_back);
         }
 
-        static void EditaItem(int _entidade)
+        static void EditaItem(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Edita Item =======================");
-            Menu.getInstance(1).RendeMenu();
+            GoBack(_back);
+
         }
-        static void RemoveItem(int _entidade)
+        static void RemoveItem(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Remove Item =======================");
-            Menu.getInstance(1).RendeMenu();
+            GoBack(_back);
         }
-        static void ListaItems(int _entidade)
+        static void ListaItems(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Lista Items =======================");
-            Menu.getInstance(1).RendeMenu();
+            GoBack(_back);
         }
 
         /// <summary>
         /// Mostra a cotnagem de Items
         /// </summary>
         /// <param name="_entidade">Armazem</param>
-        static void ContaItems(int _entidade)
+        static void ContaItems(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Contagem Items =======================");
             Output.MostraLinha($"Total de Items: {Inventario.getInstance(_entidade).TotalItems}");
-            Menu.getInstance(1).RendeMenu();
+            GoBack(_back);
         }
         
         /// <summary>
         /// Remove todos os Items do Armazem
         /// </summary>
         /// <param name="_entidade">Armazem</param>
-        static void RemoveItems(int _entidade)
+        static void RemoveItems(int _entidade, Menu _back)
         {
             Output.MostraLinha("======================= Remove Items =======================");
             Inventario.getInstance(_entidade).RemoveAll();
-            Input.LeString("Todos os Items foram Removidos ... \nPrima enter para continuar...");
-            Menu.getInstance(1).RendeMenu();
+            Input.LeString("Todos os Items foram Removidos ...");
+            GoBack(_back);
+        }
+        #endregion
+
+        #region ===============================================LOCALIZACOES
+        static void AdicionaLocalizacao()
+        {
+            Output.MostraLinha("======================= Adiciona Localização =======================");
+            GoBack(Menu.getInstance(2));
+        }
+        static void RemoveLocalizacao()
+        {
+            Output.MostraLinha("======================= Remove Localização =======================");
+            GoBack(Menu.getInstance(2));
+        }
+        static void AlteraLocalizacao()
+        {
+            Output.MostraLinha("======================= Altera Localização =======================");
+            GoBack(Menu.getInstance(2));
+        }
+        static void ListaLocalizacao()
+        {
+            Output.MostraLinha("======================= Altera Localização =======================");
+            GoBack(Menu.getInstance(2));
+        }
+        #endregion
+
+        #region ===============================================PESQUISAS
+        static void PesquisaNome(int _entidade, Menu _back)
+        {
+            Output.MostraLinha($"======================= Pesquisa Por Nome; Armazem:{_entidade} =======================");
+            GoBack(_back);
+        }
+        static void PesquisaId(int _entidade, Menu _back)
+        {
+            Output.MostraLinha($"======================= Pesquisa Por ID; Armazem:{_entidade} =======================");
+            GoBack(_back);
+        }
+        static void PesquisaFabrico(int _entidade, Menu _back)
+        {
+            Output.MostraLinha($"======================= Pesquisa Por Fabrico; Armazem:{_entidade} =======================");
+            GoBack(_back);
+        }
+        static void PesquisaTipo(int _entidade, Menu _back)
+        {
+            Output.MostraLinha($"======================= Pesquisa Por Tipo; Armazem:{_entidade} =======================");
+            GoBack(_back);
+        }
+        #endregion
+
+        static void GoBack(Menu _back, string _texto= "Prima qualquer tecla para terminar.")
+        {
+            Input.LeString(_texto);
+            _back.RendeMenu();
         }
     }
 }
